956351f4886d577c149e5c7922bf416a081e7273
Joschua Becker <jb@scolasti.co>
2024-09-04 12:17:26 +0200

    fix: removed non- existing export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { TecsafeApi as tsApiImport } from "./TecsafeApi";
 
 export * from "./TecsafeApi";
 export * from "./types/BaseWidget";
-export * from "./types/FullscreenWidget";
 export * from "./types/Messages";
 export * from "./types/Config";
 export * from "./widget/AppWidget";
